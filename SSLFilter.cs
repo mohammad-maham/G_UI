28ee36d91e6da0543b8640a58342425c244c578c
Mehrdad Najafalizadeh <najafalizadehmehrdad@gmail.com>
2024-09-17 16:31:13 +0430

    PWA Completed.

---
+++ SSLFilter.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace G_APIs
+{
+    public class SSLFilter : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (!filterContext.HttpContext.Request.IsSecureConnection)
+            {
+                string url = filterContext.HttpContext.Request.Url.ToString().Replace("http:", "https:");
+                filterContext.Result = new RedirectResult(url);
+            }
+        }
+    }
+}
